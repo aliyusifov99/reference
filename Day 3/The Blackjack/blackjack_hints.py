6230a532b7e91dc0c3e034352a4f6ba808730fbc
Ali Yusifov <72607596+aliyusifov99@users.noreply.github.com>
2022-04-26 20:14:40 +0400

    Add files via upload
    New files uploaded

---
+++ Day 3/The Blackjack/blackjack_hints.py
@@ -0,0 +1,38 @@
+# Step 1: Create a function that uses the list of cards to return a random card
+
+
+# Step 3: Create a function called calculate_score() that takes in a list of cards and returns the total score
+
+    # Step 4: Inside calculate_score() check for a blackjack (2 cards with a score of 21) and return 0 instead of actual score. 0 will
+    #  represent Blackjack in the game.
+    
+    # Step 5: Inside calculate_score() check for an ace (11).If the score is already over 21, remove 11 and replace it with 1. hint: remove()
+   
+
+# Step 10:  Create a function called compare() and pass in the user_score() and computer_score(). If the computer and user both have 
+#  the same score, then its a draw. If the computer has a blackjack (0), then the user loses. If the user has a blackjack (0),
+#  then the user wins. If the user_score is over 21, then the user loses. If the computer_score is over 21, then the computer loses.
+#  If none of the above, then the player with the highest score wins.
+
+
+
+    # Step 12: Add the art.py logo
+   
+    # Step 2: Give the user and computer 2 cards each using deal_card():
+ 
+
+    
+    # Step 8: Check the score with every new card drawn and repeat steps 6 and 7 until the game is over.
+    
+
+        # Step 6: call calculate_score(). If the computer or the user has a blackjack (0) or if the user's score is over 21, end the game.
+        
+
+            # Step 7: If the game is not over, ask the user if they want to hit or stay.
+           
+
+    # Step 9: Once the user is done, let the computer play. The computer should draw cards until the score is over 17.  
+    
+
+# Step 11: Ask the user if they want to play again. If they type "y", then clean the console, start a new game and show the logo from art.py.
+#  If they type "n", then exit the game.
